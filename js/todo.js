b55603bbef09df302067fafa4f57fbffe8b7b3a8
mert özdemir <mertozdemir91@hotmail.com>
2021-10-26 10:37:09 +0300

    gorevler eklendi

--- js/todo.js
+++ js/todo.js
@@ -2,28 +2,52 @@
 let gorevler = [
     {
         baslik:"Okula git",
-        yapildi: false
+        yapildi: true
     },
     {
         baslik:"Alışveriş yap",
-        yapildi: true
+        yapildi: false
     }
 ];
 
 const ulListe = document.getElementById("liste");
+const frmGorev = document.getElementById("gorevForm");
+const txtBaslik = document.getElementById("baslik");
+
+frmGorev.onsubmit = function(event){
+    event.preventDefault();
+    const yeniGorev ={
+        baslik: txtBaslik.value,
+        yapildi: false
+    }
+    gorevler.push(yeniGorev)
+    Listele();
+    this.reset();
+}
 
 
 function Listele(){
     
+    ulListe.innerHTML="";
     for(const gorev of gorevler){
         const li = document.createElement("li");
+        li.classList.add(gorev.yapildi ? "yapildi" : "yapilmadi");
         li.gorev = gorev
         li.textContent = gorev.baslik
+        
         const cbox = document.createElement("input");
+        cbox.checked = gorev.yapildi;
+        cbox.onchange = durumDegistiginde;
         cbox.setAttribute("type","checkbox");
         li.prepend(cbox);
         ulListe.append(li);
     }
+}
+
+function durumDegistiginde(event){
+    const gorev = this.parentElement.gorev;
+    gorev.yapildi = this.checked;
+    Listele();
 }
 
 Listele();
