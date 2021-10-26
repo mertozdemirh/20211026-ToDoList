c1d18bfdd39558a6fec03348c4fa3abb1166d536
mert özdemir <mertozdemir91@hotmail.com>
2021-10-26 11:21:22 +0300

    css duzenlendi

--- js/todo.js
+++ js/todo.js
@@ -33,17 +33,29 @@ function Listele(){
         const li = document.createElement("li");
         li.classList.add(gorev.yapildi ? "yapildi" : "yapilmadi");
         li.gorev = gorev
-        li.textContent = gorev.baslik
+       
+        const span = document.createElement("span");
+        span.textContent= gorev.baslik;
         
         const cbox = document.createElement("input");
         cbox.checked = gorev.yapildi;
         cbox.onchange = durumDegistiginde;
         cbox.setAttribute("type","checkbox");
+
+        const btnSil = document.createElement("button");
+        btnSil.onsubmit=sileTiklandiginda;
+        btnSil.textContent="SİL";
+
+        li.append(span);
         li.prepend(cbox);
+        li.append(btnSil);
         ulListe.append(li);
     }
 }
 
+function sileTiklandiginda(event){
+    console.log("sile tıklandı.")
+}
 function durumDegistiginde(event){
     const gorev = this.parentElement.gorev;
     gorev.yapildi = this.checked;
